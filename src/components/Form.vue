933b16bd816eb2a3189be97425118b333d4f4278
Anthony <anthonymaia00@gmail.com>
2024-03-07 16:21:53 -0300

    finalizando projeto

---
+++ src/components/Form.vue
@@ -0,0 +1,23 @@
+<script setup>
+    const props = defineProps(['newTask', 'temporaryTask', 'changeFilter', 'changeTaskTemp'])
+</script>
+
+<template>
+    <form @submit.prevent="props.newTask" >
+        <div class="row">
+            <div class="col">
+                <input :value="props.temporaryTask" @change="props.changeTaskTemp" required type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
+            </div>
+            <div class="col-md-2">
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
+            </div>
+            <div class="col-md-2">
+                <select @change="props.changeFilter" class="form-control">
+                    <option value="todas">Todas tarefas</option>
+                    <option value="pendentes">Pendentes</option>
+                    <option value="finalizadas">Finalizadas</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<script setup>
+    const props = defineProps(['PendingTask']);
+</script>
+
+<template>
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+        <h1>Minhas Tarefas</h1>
+        <p>
+            Você possui {{ props.PendingTask }} tarefas pendentes
+        </p>
+    </header>
+</template>
